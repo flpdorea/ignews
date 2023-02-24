cc74a74ab8539f6459b143aecf0e6b3d0311d264
flpdorea <felipeldorea@gmail.com>
2023-02-24 01:58:10 -0300

    scoped scss example

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import styles from '../styles/home.module.scss'
 export default function Home() {
   return (
     <>
-      <h1 className={styles.title}>Hello world!</h1>
+      <h1 className={styles.title}>Hello <span>world</span>!</h1>
     </>
   )
 }
