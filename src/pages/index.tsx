6993da5848f4e17e16352d9dec9ffc5c0ca50f5a
flpdorea <felipeldorea@gmail.com>
2023-02-24 03:01:21 -0300

    add Roboto font and page title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,13 @@
+
+import Head from 'next/head'
 import styles from '../styles/home.module.scss'
 
 export default function Home() {
   return (
     <>
+      <Head>
+        <title>Home | ig.news</title>
+      </Head>
       <h1 className={styles.title}>Hello <span>world</span>!</h1>
     </>
   )
